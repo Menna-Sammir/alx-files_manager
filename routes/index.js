3bc02ce99dab2b5b69b2f132fad1a97c14c6a3f9
manonaSamir <bena.yalla@yahoo.com>
2024-09-05 12:06:32 +0300

    auth

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { getStatus, getStats } = require('../controllers/AppController');
 const { postNew, getMe, getDisconnect } = require('../controllers/UsersController');
-// const { getConnect } = require('../controllers/AuthController');
+const { getConnect } = require('../controllers/AuthController');
 // const postUpload = require('../controllers/FilesController');
 
 const router = express.Router();
@@ -11,9 +11,9 @@ router.get('/stats', getStats);
 
 router.post('/users', postNew);
 
-// router.get('/connect', getConnect);
-// router.get('/users/me', getMe);
-// router.get('/disconnect', getDisconnect);
+router.get('/connect', getConnect);
+router.get('/users/me', getMe);
+router.get('/disconnect', getDisconnect);
 
 // router.post('/files', postUpload);
 
